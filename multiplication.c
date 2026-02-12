0695e2050fe0f3cd2c48d423f73231de2364b0db
rohan-kulkarni21 <rohan.kulkarni.2107@gmail.com>
2026-02-12 02:12:02 -0800

    multiplication using c

---
+++ multiplication.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+int a,b,product;
+
+printf("enter your number");
+scanf("%d", &a);
+printf("enter your second number");
+scanf("%d",&b);
+
+product=a+b;
+
+printf("product of both number is %d", product);
+
+return 0;
+} 
