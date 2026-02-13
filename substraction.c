d7cb4e0c34229db249378137e66d9b178fcfbc48
rohan-kulkarni21 <rohan.kulkarni.2107@gmail.com>
2026-02-12 18:54:13 -0800

    substraction using c

---
+++ substraction.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+int a,b, diff;
+
+printf("enter first number");
+scanf("%d", &a);
+
+printf("enter second number");
+scanf("%d", &b);
+
+
+diff=a-b;
+
+printf("the difference between two number is %d", diff);
+
+return0;
+}
