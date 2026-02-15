138523b56a7e2105627c7f12942172b3b5afe1df
rohan-kulkarni21 <rohan.kulkarni.2107@gmail.com>
2026-02-15 00:37:38 -0800

    greatest of two number

---
+++ greatest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+int a,b;
+
+printf("enter first number");
+scanf("%d", &a);
+printf("enter second number");
+scanf("%d",&b);
+
+if(a>b){
+printf("a is largest number");
+}
+else{
+printf("b is largest number");
+}
+
+return 0;
+}
