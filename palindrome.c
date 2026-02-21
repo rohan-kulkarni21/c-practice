38ec12c848744cfd8ddadd2f24136843529ff0bd
rohan-kulkarni21 <rohan.kulkarni.2107@gmail.com>
2026-02-20 23:32:09 -0800

    check number if palindrome

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int num, reverse = 0, remainder, original;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while(num != 0) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+    if(original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
