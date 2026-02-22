67b1b5dad338f7395c5477345e6f97a381c0e4b1
rohan-kulkarni21 <rohan.kulkarni.2107@gmail.com>
2026-02-21 17:44:40 -0800

    average using array

---
+++ array_average.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    int a[5], i, sum = 0;
+    float avg;
+
+    printf("Enter 5 numbers:");
+
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+
+    avg = sum / 5.0;
+
+    printf("Average = %.2f", avg);
+
+    return 0;
+}
+
+
